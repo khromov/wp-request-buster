3b466df9f9ead0e6c21275a5201b4b3ae4b57194
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2014-12-09 00:10:59 +0100

    Github updater support

--- request-buster.php
+++ request-buster.php
@@ -7,6 +7,8 @@ Version: 2014.09.23.2
 Author: khromov
 Author URI: https://profiles.wordpress.org/khromov
 License: GPL2
+GitHub Plugin URI: https://github.com/khromov/wp-request-buster
+GitHub Branch:     master
 */
 
 $request_buster_requests = array();
